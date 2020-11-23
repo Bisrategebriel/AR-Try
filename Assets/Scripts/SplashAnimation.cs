beaed0178e6e52f9b1e463e44275d9f2cdd2fe70
Bisrategebriel Fisseha <42300234+Bisrategebriel@users.noreply.github.com>
2020-11-23 18:01:38 +0300

    Add files via upload

---
+++ Assets/Scripts/SplashAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadMainScreen());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadMainScreen(){
+        yield return new WaitForSeconds(5f);
+
+        SceneManager.LoadScene("SampleScene");
+    }
+}
